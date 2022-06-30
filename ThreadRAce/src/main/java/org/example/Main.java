fce9390a535d1a21c5fbe4e978aaeb3eb7b6ba69
Suleyman <61500642+suleymanakay@users.noreply.github.com>
2022-06-30 23:17:54 +0300

    Add files via upload

---
+++ ThreadRAce/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService execute = Executors.newFixedThreadPool(4);
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            array.add(i);
+        }
+
+        ThreadRace thr = new ThreadRace(array);
+
+        execute.execute(thr);
+
+    }
+}
---
+++ ThreadRAce/src/main/java/org/example/ThreadRace.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class ThreadRace extends Thread{
+    private ArrayList<Integer> odd = new ArrayList<>();
+    private ArrayList<Integer> even = new ArrayList<>();
+    private ArrayList<Integer> general = new ArrayList<>();
+
+    public ThreadRace(ArrayList<Integer> general) {
+        this.general = general;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < general.size(); i++) {
+
+            if (general.get(i) % 2 == 0) {
+                even.add(general.get(i));
+            } else {
+                odd.add(general.get(i));
+            }
+
+        }
+
+        printOdd();
+        printEven();
+
+    }
+
+    public synchronized void printOdd() {
+        System.out.println(this.odd);
+
+    }
+
+    public synchronized void printEven() {
+        System.out.println(this.even);
+    }
+
+}
